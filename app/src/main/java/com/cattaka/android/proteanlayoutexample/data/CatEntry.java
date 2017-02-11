63ddc7adccb10daa91c8985bebcc629b21f05d74
Takao Sumitomo <cattaka@mail.cattaka.net>
2017-02-12 02:31:12 +0900

    Add data class

---
+++ app/src/main/java/com/cattaka/android/proteanlayoutexample/data/CatEntry.java
@@ -0,0 +1,26 @@
+package com.cattaka.android.proteanlayoutexample.data;
+
+/**
+ * Created by cattaka on 17/02/12.
+ */
+
+public class CatEntry {
+    private String name;
+    private String file;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+}
