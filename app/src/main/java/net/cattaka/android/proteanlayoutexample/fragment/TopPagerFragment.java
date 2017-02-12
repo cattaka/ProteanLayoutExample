f1651e0bef0ee1bf1a4d989484a37153004b68e1
Takao Sumitomo <cattaka@mail.cattaka.net>
2017-02-12 21:34:44 +0900

    Add fab

--- app/src/main/java/net/cattaka/android/proteanlayoutexample/fragment/TopPagerFragment.java
+++ app/src/main/java/net/cattaka/android/proteanlayoutexample/fragment/TopPagerFragment.java
@@ -3,9 +3,11 @@ package net.cattaka.android.proteanlayoutexample.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import net.cattaka.android.proteanlayoutexample.R;
 import net.cattaka.android.proteanlayoutexample.adapter.factory.TopPagerAdapter;
@@ -16,6 +18,24 @@ import net.cattaka.android.proteanlayoutexample.databinding.FragmentTopPagerBind
  */
 
 public class TopPagerFragment extends Fragment {
+    ViewPager.OnPageChangeListener mOnPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            float factor = (position == 0) ? positionOffset : 1;
+            mBinding.setScrollFactor(factor);
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
     FragmentTopPagerBinding mBinding;
 
     @Nullable
@@ -28,8 +48,14 @@ public class TopPagerFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mBinding = FragmentTopPagerBinding.bind(view);
+        mBinding.setFragment(this);
 
         TopPagerAdapter adapter = new TopPagerAdapter(getChildFragmentManager());
         mBinding.viewPager.setAdapter(adapter);
+        mBinding.viewPager.addOnPageChangeListener(mOnPageChangeListener);
+    }
+
+    public void onClickSearch(View view) {
+        Toast.makeText(getContext(), "Not implemented yet", Toast.LENGTH_SHORT).show();
     }
 }
