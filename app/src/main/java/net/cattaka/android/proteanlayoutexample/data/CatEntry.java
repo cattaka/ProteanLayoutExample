28019cee5dc08a3f6087f0b9293ba8a792fbe857
Takao Sumitomo <cattaka@mail.cattaka.net>
2017-02-20 20:23:19 +0900

    Add data

--- app/src/main/java/net/cattaka/android/proteanlayoutexample/data/CatEntry.java
+++ app/src/main/java/net/cattaka/android/proteanlayoutexample/data/CatEntry.java
@@ -7,6 +7,10 @@ package net.cattaka.android.proteanlayoutexample.data;
 public class CatEntry {
     private String name;
     private String file;
+    private String when;
+    private String where;
+    private String hair;
+    private String color;
 
     public String getName() {
         return name;
@@ -22,5 +26,37 @@ public class CatEntry {
 
     public void setFile(String file) {
         this.file = file;
+    }
+
+    public String getWhen() {
+        return when;
+    }
+
+    public void setWhen(String when) {
+        this.when = when;
+    }
+
+    public String getWhere() {
+        return where;
+    }
+
+    public void setWhere(String where) {
+        this.where = where;
+    }
+
+    public String getHair() {
+        return hair;
+    }
+
+    public void setHair(String hair) {
+        this.hair = hair;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
